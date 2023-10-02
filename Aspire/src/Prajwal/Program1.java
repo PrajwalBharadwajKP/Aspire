ae3899d85b79d1044c10540825a6b9270350e6bc
Trisulam R <thrisulam008@gmail.com>
2023-10-02 10:30:07 +0530

    contribuution code Line 10

--- Aspire/src/Prajwal/Program1.java
+++ Aspire/src/Prajwal/Program1.java
@@ -1,7 +1,12 @@
 package Prajwal;
 
 public class Program1 {
-	public void m1() {
+	
+	public static void main(String [] args) {
+		
 		System.out.println("code by prajwal");
+		
+		//contribution has been made to the code 
+		System.out.println("contributed by Triulam R");
 	}
 }
