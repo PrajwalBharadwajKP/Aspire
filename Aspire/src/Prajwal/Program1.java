7c71b6f9e7fde9c378f408cc09f0cbb4f1f26d3e
Prajwal <prajwalbharadwaj14@gmail.com>
2023-10-03 12:40:53 +0530

    new changes

--- Aspire/src/Prajwal/Program1.java
+++ Aspire/src/Prajwal/Program1.java
@@ -1,34 +1,21 @@
-package Prajwal;
+package src.Prajwal;
 
 public class Program1 {
-	
 	public static void main(String [] args) {
-		
 		System.out.println("code by prajwal");
 		System.out.println("code by Ayyappa-Naga");
-		
-
-		
 		//contribution has been made to the code 
 		System.out.println("contributed by Triulam R");
-		
 		System.out.println("code by thejas");
-		
 	}
 	public void madhu() {
 		System.out.println("code by madhu");
 	}
 	public void v1() {
-		System.out.println("from Vinod");}
-
-		public void v2() {
-			System.out.println("from Ayyappa-Naga");
-			System.out.println("from Amruth");
-		
+		System.out.println("from Vinod");
 	}
-
-	
+	public void v2() {
+		System.out.println("from Ayyappa-Naga");
+		System.out.println("from Amruth");
+	}
 }
-
-}
-
