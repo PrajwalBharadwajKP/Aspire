f2caa7856e5298f0556d2ab9c90b3be1eb848002
Prajwal <prajwalbharadwaj14@gmail.com>
2023-10-04 10:51:33 +0530

    code pushed by prajwal

---
+++ AspireGithub/src/Practice/Program1.java
@@ -0,0 +1,8 @@
+package Practice;
+
+public class Program1 {
+	//please create your own method and then write the sopln statement inside it
+	public void prajwal() {
+		System.out.println("code added by prajwal");
+	}
+}
